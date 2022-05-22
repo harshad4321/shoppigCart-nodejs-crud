187946ce45ec52db73fcc68824867bec345c1b74
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-05-22 15:52:50 +0530

    fix the cart butten

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -10,18 +10,16 @@ var objectId=require('mongodb').ObjectId
 module.exports={
     doSignup:(userData)=>{
         return new Promise (async(resolve,reject)=>{
-         userData.password=  salt=   await bcrypt.genSalt(10);
-             await  bcrypt.hash(userData.password,10,(err, password,hash) => {
-                if(err) throw (err);
-                 else
-                password=hash
+      
+        userData.password=await bcrypt.hash(userData.password,10)
+               
                   db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data)=>{
                     resolve(data) 
                     // console.log(userData.password)
                     // console.log(userData.email)                    
                 });
                 });
-            }); 
+             
      },
         
      doLogin:(userData)=>{
@@ -29,87 +27,86 @@ module.exports={
             let loginStatus = false
             let response = {}
             let user=await db.get().collection(collection.USER_COLLECTION).findOne({ email:userData.email })
-            .then((user)=>{
-
-            //  if (user) {
+             if (user) {
 // check if password matches    
-           await bcrypt.compare(userData.password,user.password ,(err,match,next)=> {
+       bcrypt.compare(userData.password,user.password).then((status)=> {
                 console.log(user.password)
                 console.log(userData.password)
-               
-                
-            if (err) throw err; 
+              
+                    if(status){
+                        console.log("login success...");
+                        response.user=user
+                        response.status=true
+                        resolve(response)
 
-                      else {
-                        if (!match) {
-                            console.log('login failed try again');
-                        // resolve({status: false})
-                        } else {
-                           console.log("login success...");
-                       // response.user=user
-                       // response.status=true
-                       // resolve(response)
-                        }
-                    } 
+                    }else{
+
+                        console.log('login failed');
+                        resolve({status:false})
+
+                    }
                 })
-              })// else {
-            //     console.log("login failed because email is not there..")
-            //     //     resolve({status: false})
-            // }
+            }else{
+                console.log("login falied because email not there..")
+                resolve({status: false})
+            }
         })
-    
-    },
-   addToCart:(proId,userId)=>{
-       return new Promise(async(resolve,reject)=>{
-           let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:ObjectId(userId)})
-           if(userCart){
-               db.get().collection(collection.CART_COLLECTION)
-               .updateOne({user:objectId(userId)},
-                {
- 
-                        $push:{products:objectId(proId)}
-               
-                }
-               ).then((response)=>{
-                   resolve()
-               })
-           }else{
-               let cartObj={
-                   user:objectId(userId),
-                   products:[objectId(proId)]
-               }  
-               db.get().collection(collection.CART_COLLECTION).insertOne (cartObj).then((response)=>{
-                   resolve()
-               })
-           }
 
-       })
-   },
-  getCartProducts:(userId)=>{
-      return new Promise(async(resolve,reject)=>{
-          let cartItem=await db.get().collection(collection.CART_COLLECTION).aggregate([
-              {
-                  $match:{user:objectId(userId)}
-              },{
-                  $lookup:{
-                      from:collection.PRODUCT_COLLECTION,
-                      let:{prodList:'$products'},
-                      pipeline:[
-                          {
-                              $match:{
-                                  $expr:{
-                                      $in:['$_id',"$$prodList"]
-                                  }
-                              }
-                          }
-                      ],
-                      as:'cartItems'
-                  }
-              }
-          ]).toAarray()
-          resolve(cartItems[0].cartItems )
-      })
-  }
-
+    }
+  
 }
+
+//    addToCart:(proId,userId)=>{
+//        return new Promise(async(resolve,reject)=>{
+//            let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:ObjectId(userId)})
+//            if(userCart){
+//                db.get().collection(collection.CART_COLLECTION)
+//                .updateOne({user:objectId(userId)},
+//                 {
+ 
+//                         $push:{products:objectId(proId)}
+               
+//                 }
+//                ).then((response)=>{
+//                    resolve()
+//                })
+//            }else{
+//                let cartObj={
+//                    user:objectId(userId),
+//                    products:[objectId(proId)]
+//                }  
+//                db.get().collection(collection.CART_COLLECTION).insertOne (cartObj).then((response)=>{
+//                    resolve()
+//                })
+//            }
+
+//        })
+//    },
+//   getCartProducts:(userId)=>{
+//       return new Promise(async(resolve,reject)=>{
+//           let cartItem=await db.get().collection(collection.CART_COLLECTION).aggregate([
+//               {
+//                   $match:{user:objectId(userId)}
+//               },{
+//                   $lookup:{
+//                       from:collection.PRODUCT_COLLECTION,
+//                       let:{prodList:'$products'},
+//                       pipeline:[
+//                           {
+//                               $match:{
+//                                   $expr:{
+//                                       $in:['$_id',"$$prodList"]
+//                                   }
+//                               }
+//                           }
+//                       ],
+//                       as:'cartItems'
+//                   }
+//               }
+//           ]).toAarray()
+//           resolve(cartItems[0].cartItems )
+//       })
+//   }
+
+// }
  
--- routes/user.js
+++ routes/user.js
@@ -7,18 +7,32 @@ const productHelpers = require('../helpers/product-helpers');
 const userHelpers = require("../helpers/user-helpers");
 const multer = require('multer')
 
+const verifyLogin=(req,res,next)=>{
+   if (req.session.loggedIn){
+    next()
+   }else{
+   res.redirect('/login')
+   }
+}
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-//  let user = req.session.user
-//  console.log(user)
+ let user = req.session.user
+ console.log(user) 
  productHelpers.getAllProducts().then((products)=>{
      
-  res.render('user/view-product',{products})   
+  res.render('user/view-product',{products,user})   
    });
 });
-router.get('/login',(req,res)=>{            
-  res.render('user/login')
+router.get('/login',(req,res)=>{
+   if (req.session.loggedIn){
+      res.redirect('/')
+   }else {
+
+      res.render('user/login',{"loginErr":req.session.loginErr})
+      req.session.loginErr=null
+   }          
+
 });
 
 router.get('/signup',(req,res)=>{
@@ -33,34 +47,31 @@ router.post('/signup',(req,res)=>{
    })
 });
 router.post('/login',(req,res)=>{
-   userHelpers.doLogin(req.body)
-}); 
-   //   console.log(req.body) //.then((response)=>{   
-      //    console.log(response);
-      //   console.log(req.body);
-      //    if(response.status){
-      //       req.session.loggedIn=true 
-      //       req.session.user=response.user 
-      //       res.redirect('/')
-      //    }else{
-      //       //  req.session.loginErr="Invalid username or password"
-      //       res.redirect('/login')
-      //    }
-      // });
+   userHelpers.doLogin(req.body).then((response)=>{ 
+
+         if(response.status){
+            req.session.loggedIn=true 
+            req.session.user=response.user 
+            res.redirect('/')
+         }else{
+             req.session.loginErr="Invalid username or password"
+            res.redirect('/login')
+         }
+      });
+   });  
+
+router.get('/logout',(req,res)=>{
+   req.session.destroy()
+   res.redirect('/')
+})
+router.get('/cart',verifyLogin,async(req,res)=>{
+   let products =await userHelpers.getCartProducts(req.session.user._id)
+   console.log(products);
+ res.render('user/cart')
+})
 
 
-// router.get('/logout',(req,res)=>{
-//    req.session.destroy()
-//    res.redirect('/')
-// })
-// router.get('/cart',verifyLogin,async(req,res)=>{
-//    let products =await userHelpers.getCartProducts(req.session.user._id)
-//    console.log(products);
-//  res.render('user/cart')
-// })
-
-
-router.get('/cart',(req,res)=>{
+router.get('/cart',verifyLogin,(req,res)=>{
    res.render(`user/cart`)
 })
 router.get('/add-to-cart/:id',(req,res)=>{
@@ -68,6 +79,7 @@ userHelpers.addToCart(req.params.id,req.seccion.user._id).then(()=>{
    res.redirect('product')
 })
 })
+
 
 module.exports = router;
 
