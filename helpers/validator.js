e4d4859a6f058be5bbd7c719728b62cd899708f9
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-08-10 01:32:50 +0530

    signup page set  password verifiction is set

--- helpers/validator.js
+++ helpers/validator.js
@@ -5,10 +5,25 @@ const userSignUpValidationRules = () => {
     check("name", "Name is required").not().isEmpty(),
     check("name", "at least 4 character required,please enter your full Name").isLength({ min: 4 }),
     check("email", "Invalid email").not().isEmpty().isEmail(),
-    check("password", "Please enter a password with 4 or more characters")
+    check("password")
       .not()
       .isEmpty()
-      .isLength({ min: 4 }),
+      .isLength({ min: 4 })
+      .withMessage('Please enter a password with 4 or more characters'),
+     
+      check("confirm_password")
+      .not()
+      .isEmpty()
+      .isLength({ min: 4 })
+      .withMessage('Passwords must match.')
+    .custom((value,{req})=>{
+      if(value !==req.body.password)
+      {  // trow error if passwords do not match
+        throw new Error("Passwords don't match,please enter correct password");
+      }else {
+        return value;
+    }
+    })
   ];
 };
 const userSignInValidationRules = () => {
@@ -28,11 +43,14 @@ const userContactUsValidationRules = () => {
     check("message", "Please enter a message with at least 10 words")
       .not()
       .isEmpty()
-      .isLength({ min: 10 }),
+      .isLength({ min: 10 })
+      
   ];
 };
 
 const validateSignup = (req, res, next) => {
+  
+    
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     var messages = [];
@@ -46,6 +64,7 @@ const validateSignup = (req, res, next) => {
 };
 
 const validateSignin = (req, res, next) => {
+  
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     var messages = [];
--- routes/user.js
+++ routes/user.js
@@ -61,6 +61,7 @@ router.get('/signup',(req,res)=>{
 router.post(
    '/signup',
    [
+      
       userSignUpValidationRules(),
       validateSignup,
    ],
