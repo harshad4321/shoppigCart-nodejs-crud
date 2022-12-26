8c3e1eda01db1a301425439366ba96f08f1eaa52
Harshad.N <harshad.n4321@gmail.com>
2022-12-26 10:13:16 +0530

    deploy

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,14 @@
 
-  function addToCart(proId) {
-    $.ajax({
-      url: '/add-to-cart/' + proId,
-      method: 'get',
-      success: (response) => {
-          if(response.status){
-              let count=$('#cart-count').html()
-              count=parseInt(count)+1
-              $("#cart-count").html(count)
-          }
-        
+function addToCart(proId) {
+  $.ajax({
+    url: '/add-to-cart/' + proId,
+    method: 'get',
+    success: (response) => {
+      if (response.status) {
+        let count = $('#cart-count').html()
+        count = parseInt(count) + 1
+        $("#cart-count").html(count)
       }
-    })
-  }
+    }
+  })
+}
